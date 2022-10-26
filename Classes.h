e7f0007350fcb5d0e0b682745b2cf615684c6c06
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-26 18:11:08 +0300

    Update Classes.h

--- Classes.h
+++ Classes.h
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+void cwop() { cout << "SYSTEM MESSAGE!" << endl << "CONSTRUCTOR WITHOUT PARAMETRES WAS CALLED!" << endl << endl; }
+void cwp() { cout << "SYSTEM MESSAGE!" << endl << "CONSTRUCTOR WITH PARAMETRES WAS CALLED!" << endl << endl; }
+void cc() {	cout << "SYSTEM MESSAGE!" << endl << "COPYING CONSTRUCTOR WAS CALLED!" << endl << endl; }
+void dest() { cout << "SYSTEM MESSAGE!" << endl << "DESTRUCTOR WAS CALLED!" << endl << endl; }
 class Heroes
 {
 public:
@@ -16,72 +20,23 @@ class Friends : public Heroes
 {
 	//name, weapon, xp
 public:
-	Friends()
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "CONSTRUCTOR WITHOUT PARAMETRES WAS CALLED!" << endl << endl;
-	}
+	Friends() { cwop(); }
 	Friends(string name, string weapon)
 	{
 		this->name = name;
 		this->weapon = weapon;
 		this->xp = xp;
-
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "CONSTRUCTOR WITH PARAMETRES WAS CALLED!" << endl << endl;
+		cwp();
 	}
-	Friends(const Friends& obj)
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "COPYING CONSTRUCTOR WAS CALLED!" << endl << endl;
-	}
-	~Friends()
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "DESTRUCTOR WAS CALLED!" << endl << endl;
-	}
+	Friends(const Friends& obj) { cc(); }
+	~Friends() { dest(); }
 	
-	int size() { return n; }
-	void setsize(int s) { n = s; }
-
-	void get()
-	{
-		cout << "Имя: " << name << endl;
-		cout << "Оружие: " << weapon << endl;
-		cout << "Навыки: " << xp << endl;
-	}
-	
-	void set(string h, string w, string x)
-	{
-		name = h;
-		weapon = w;
-		xp = x;
-	}
-
-	void add()
-	{
-		n++;
-		string h, w, x;
-		cout << "Имя: ";
-		cin >> h;
-		cout << "Оружие: ";
-		cin >> w;
-		cout << "Навыки: ";
-		cin >> x;
-		set(h, w, x);
-	}
-
-	void edit()
-	{
-		string h, w, x;
-		cout << "Имя: ";
-		cin >> h;
-		cout << "Оружие: ";
-		cin >> w;
-		cout << "Навыки: ";
-		cin >> x;
-		set(h, w, x);
-	}
+	int size();
+	void setsize(int s);
+	void get();
+	void set(string h, string w, string x);
+	void add();
+	void edit();
 
 };
 
@@ -91,11 +46,7 @@ class Enemies : public Heroes
 	string home;
 	//name, weapon, xp
 public:
-	Enemies()
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "CONSTRUCTOR WITHOUT PARAMETRES WAS CALLED!" << endl << endl;
-	}
+	Enemies() { cwop(); }
 	Enemies(string name, string weapon, string crime, string home, string xp)
 	{
 		this->name = name;
@@ -103,63 +54,17 @@ public:
 		this->crime = crime;
 		this->home = home;
 		this->xp = xp;
-
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "CONSTRUCTOR WITH PARAMETRES WAS CALLED!" << endl << endl;
+		cwp();
 	}
-	Enemies(const Enemies& obj)
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "COPYING CONSTRUCTOR WAS CALLED!" << endl << endl;
-	}
-	~Enemies()
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "DESTRUCTOR WAS CALLED!" << endl << endl;
-	}
+	Enemies(const Enemies& obj) { cc(); }
+	~Enemies() { dest(); }
 	
-	int size() { return n; }
-	void setsize(int s) { n = s; }
-
-	void get()
-	{
-		cout << "Имя: " << name << endl;
-		cout << "Оружие: " << weapon << endl;
-		cout << "Преступление: " << crime << endl;
-		cout << "Локация: " << home << endl;
-		cout << "Навыки: " << xp << endl;
-	}
-
-	void set(string n, string w, string c, string h, string x)
-	{
-		name = n;
-		weapon = w;
-		crime = x;
-		home = h;
-		xp = x;
-	}
-
-	void add()
-	{
-		string n, w, c, h, x;
-		cout << "Имя: "; cin >> h;
-		cout << "Оружие: ";	cin >> w;
-		cout << "Преступление: "; cin >> c;
-		cout << "Локация: "; cin >> h;
-		cout << "Навыки: "; cin >> x;
-		set(h, w, c, h, x);
-	}
-
-	void edit()
-	{
-		string n, w, c, h, x;
-		cout << "Имя: "; cin >> h;
-		cout << "Оружие: ";	cin >> w;
-		cout << "Преступление: "; cin >> c;
-		cout << "Локация: "; cin >> h;
-		cout << "Навыки: "; cin >> x;
-		set(h, w, c, h, x);
-	}
+	int size();
+	void setsize(int s);
+	void get();
+	void set(string n, string w, string c, string h, string x);
+	void add();
+	void edit();
 };
 
 class Monsters : public Heroes
@@ -167,50 +72,20 @@ class Monsters : public Heroes
 	string desc;
 	//name
 public:
-	Monsters()
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "CONSTRUCTOR WITHOUT PARAMETRES WAS CALLED!" << endl << endl;
-	}
+	Monsters() { cwop(); }
 	Monsters(string name, string desc)
 	{
 		this->name = name;
 		this->desc = desc;
+		cwp();
+	}
+	Monsters(const Monsters& obj) { cc(); }
+	~Monsters() { dest(); }
 
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "CONSTRUCTOR WITH PARAMETRES WAS CALLED!" << endl << endl;
-	}
-	Monsters(const Monsters& obj)
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "COPYING CONSTRUCTOR WAS CALLED!" << endl << endl;
-	}
-	~Monsters()
-	{
-		cout << "SYSTEM MESSAGE!" << endl;
-		cout << "DESTRUCTOR WAS CALLED!" << endl << endl;
-	}
-
-	int size() { return n; }
-	void setsize(int s) { n = s; }
-
-	void get()
-	{
-		cout << "Имя: " << name << endl;
-		cout << "Описание: " << desc << endl;
-	}
-
-	void set(string n, string d)
-	{
-		name = n;
-		desc = d;
-	}
-
-	void add()
-	{
-		string n, d;
-		cout << "Имя: "; cin >> n;
-		cout << "Описание: "; cin >> d;
-		set(n, d);
-	}
+	int size();
+	void setsize(int s);
+	void get();
+	void set(string n, string d);
+	void add();
+	void edit();
 };
