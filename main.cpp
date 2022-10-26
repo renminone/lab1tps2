c14fd74f999adb30233dbff82212ea60a3afd8ca
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-26 16:11:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int main()
 	setlocale(LC_ALL, "Rus");
 	int l = 1;
 	int i, j;
-	Friends* hero = new Friends;
+	Friends hero;
+	Enemies enemy;
+	Monsters monster;
 
 	cout << "Меню:" << endl;
 	cout << "1 - Положительные герои." << endl;
@@ -21,16 +23,24 @@ int main()
 		switch (_getch())
 		{
 		case '1':
-
+			hero.add();
 			break;
 		case '2':
-
+			enemy.add();
 			break;
 		case '3':
-
+			monster.add();
 			break;
 		case '4':
-
+			cout << "Список героев: " << endl;
+			hero.get();
+			cout << endl;
+			cout << "Список злодеев: " << endl;
+			enemy.get();
+			cout << endl;
+			cout << "Список монстров: " << endl;
+			monster.get();
+			cout << endl;
 			break;
 		case '0': l = 0; break;
 		}
