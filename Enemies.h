b9b7b85f399767bdcb57d18c7931b496e31b8292
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-29 21:42:04 +0300

    Create Enemies.h

---
+++ Enemies.h
@@ -0,0 +1,34 @@
+#pragma once
+#pragma once
+#include "Heroes.h"
+
+class Enemies : public Heroes
+{
+private:
+	//Имя, оружие, преступление, локация, основные навыки.
+	string crime, home;
+public:
+	Enemies();
+	Enemies(string name, string weapon, string crime, string home, string xp);
+	~Enemies();
+
+	void set_name(string name);
+	string get_name();
+
+	void set_weapon(string weapon);
+	string get_weapon();
+
+	void set_crime(string crime);
+	string get_crime();
+
+	void set_home(string home);
+	string get_home();
+
+	void set_xp(string xp);
+	string get_xp();
+
+	string describe() override;
+	HeroesTypes get_type() override;
+
+	friend std::istream& operator>> (std::istream& in, Enemies& enemies);
+};
