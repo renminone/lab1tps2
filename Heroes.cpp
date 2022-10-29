252594a618d7c7993387ced0004b70e543589406
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-29 22:09:01 +0300

    Update Heroes.cpp

--- Heroes.cpp
+++ Heroes.cpp
@@ -8,6 +8,11 @@ Heroes::Heroes()
     cout << "\nSYSTEM MESSAGE!\nHERO CONSTRUCTOR WITHOUT PARAMETRES WAS CALLED!\n\n";
 }
 
+Heroes::Heroes(const Heroes &obj)
+{      
+    cout << "\nSYSTEM MESSAGE!\HEROES COPY CONSTRUCTOR WAS CALLED!\n\n";
+}
+
 Heroes::~Heroes()
 {
     cout << "\nSYSTEM MESSAGE!\nHERO DESTRUCTOR WAS CALLED!\n\n";
