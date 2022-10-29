f9633769a7129121df7b5dc6c63abd40f52c91dd
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-29 21:40:31 +0300

    Update and rename Classes.cpp to Heroes.cpp

---
+++ Heroes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Heroes.h"
+
+using namespace std;
+
+Heroes::Heroes()
+{
+    cout << "\nSYSTEM MESSAGE!\nHERO CONSTRUCTOR WITHOUT PARAMETRES WAS CALLED!\n\n";
+}
+
+Heroes::~Heroes()
+{
+    cout << "\nSYSTEM MESSAGE!\nHERO DESTRUCTOR WAS CALLED!\n\n";
+}
