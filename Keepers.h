5608e5bb0c2c58b05f95efb0f7a757876fce2d28
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-29 21:46:10 +0300

    Create Keepers.h

---
+++ Keepers.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include "Heroes.h"
+#include "Friends.h"
+#include "Enemies.h"
+#include "Monsters.h"
+#include <fstream>
+#include <string>
+#include <limits>
+using std::stoi;
+using std::getline;
+using namespace std;
+
+struct Node
+{
+	Heroes* heroes;
+	Node* next;
+};
+
+class Keeper
+{
+	Node* head = NULL;
+	Node* tail = NULL;
+	int n;
+public:
+	Keeper();
+	~Keeper();
+	void add(Heroes* new_hero);
+	void print();
+	void remove(int num);
+	void upload();
+	void load();
+	void free();
+	void edit(int num);
+	Heroes* get(int num);
+	int get_count_arguments(string str, string delimiter);
+	string* split(string str, string delimiter, int count);
+	int input_int(string prompt);
+	bool is_int(string str);
+
+};
