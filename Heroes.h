d63824eea2bc9daaf5f68d757f661a45e52799b0
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-10-29 21:39:51 +0300

    Update and rename Classes.h to Heroes.h

---
+++ Heroes.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+
+using std::string;
+using std::cin;
+using std::cout;
+
+class Heroes
+{
+private:
+
+public:
+    string name, weapon, xp;
+    enum HeroesTypes
+    {
+        FRIENDS = 1,
+        ENEMIES,
+        MONSTERS
+    };
+    Heroes();
+    ~Heroes();
+
+    virtual HeroesTypes get_type() = 0;
+    virtual string describe() = 0;
+};
